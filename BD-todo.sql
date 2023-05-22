0ccc66a07fcad451d2547f1e406e474fb4adc426
caea1969 <131100773+caea1969@users.noreply.github.com>
2023-05-21 23:06:28 -0500

    Add files via upload

---
+++ BD-todo.sql
@@ -0,0 +1,12 @@
+CREATE database todo;
+
+USE todo;
+CREATE TABLE tasks(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	description VARCHAR(200),
+	date DATE NOT NULL,
+	status ENUM ("PENDIENTE", "REALIZADA") default "PENDIENTE" 
+);
+
+select * from tasks;
